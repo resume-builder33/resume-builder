6d2d8f51e8d18698bd17ae529864dd28ad166218
Jung Hoan Kim <kjunghoan@gmail.com>
2024-07-27 16:32:00 -0400

    set axios route to correct endpoint

--- src/utils/postResume.ts
+++ src/utils/postResume.ts
@@ -4,11 +4,13 @@ import api from "./api"
 import { ResumeInput } from "../types.ts"
 
 export default async function postResume(message: ResumeInput) {
-  return api.post("/api", message)
+  return api.post("/generate", message)
     .then((res) => {
+      console.log("I am sending a request to the backend")
       console.log(res);
     })
     .catch((error) => {
+      console.log("oh no an error~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
       console.log(error)
     })
 
