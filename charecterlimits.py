e0055c4a558e2be41b2a88e7238eda74eaba1245
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2024-07-27 14:03:09 -0400

    wrote processing

---
+++ charecterlimits.py
@@ -0,0 +1,9 @@
+#set the charecter limit for each input iteam adjuest these later 
+
+CHARACTER_LIMITS = {
+    'name': 50,
+    'email': 100,
+    'education': 500,
+    'experience': 1000,
+    'skills': 300
+}
