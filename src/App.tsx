ad018ea9064d8959cd61edff8134180b03df1a5c
Jung Hoan Kim <kjunghoan@gmail.com>
2024-07-28 12:50:41 -0400

    removing unneeded comments and updating gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,6 @@ function App() {
 
   return (
     <>
-      <h1>navBar component  might be needed might not</h1>
-      <h1>Blurb about what this is</h1>
-      <h1>form inputs</h1>
-      <h1>submit</h1>
-      <h1>Loading animation</h1>
-      <h1>output</h1>
-      <h1>download output</h1>
-      <h1> go back to original page load</h1>
       <ResumeInputForm />
     </>
   )
